16fee4704d3d1d71921b608a39f0e11194c2cb42
stefangpetrov <stef4ogpetrov@gmail.com>
2026-09-23 21:59:19 +0300

    Complete property CRUD and permissions

--- properties/urls.py
+++ properties/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from .views import property_create, property_detail, property_list
+from .views import property_create, property_detail, property_edit, property_delete, property_list
 
 
 urlpatterns = [
     path("", property_list, name="property_list"),
     path("create/", property_create, name="property_create"),
+    path("<int:pk>/edit/", property_edit, name="property_edit"),
+    path("<int:pk>/delete/", property_delete, name="property_delete"),
     path("<int:pk>/", property_detail, name="property_detail"),
 ]
--- properties/views.py
+++ properties/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseForbidden
 
 from .models import Property
 from .forms import PropertyForm
@@ -43,3 +44,45 @@ def property_create(request):
     }
 
     return render(request, "properties/property_form.html", context)
+
+@login_required
+def property_edit(request, pk):
+    property = get_object_or_404(Property, pk=pk)
+
+    if property.owner != request.user:
+        return HttpResponseForbidden()
+
+    if request.method == "POST":
+        form = PropertyForm(request.POST, instance=property)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect("property_detail", pk=property.pk)
+
+    else:
+        form = PropertyForm(instance=property)
+
+    context = {
+        "form": form,
+        "property": property,
+    }
+
+    return render(request, "properties/property_form.html", context)
+
+@login_required
+def property_delete(request, pk):
+    property = get_object_or_404(Property, pk=pk)
+
+    if property.owner != request.user:
+        return HttpResponseForbidden()
+
+    if request.method == "POST":
+        property.delete()
+        return redirect("property_list")
+
+    context = {
+        "property": property,
+    }
+
+    return render(request, "properties/property_confirm_delete.html", context)
