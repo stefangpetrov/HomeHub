a45d3f814d677746b83ad46b6625f85c815902d1
stefangpetrov <stef4ogpetrov@gmail.com>
2026-09-25 18:01:16 +0300

    Add search/filters and pagination.

--- properties/views.py
+++ properties/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseForbidden
+from django.core.paginator import Paginator
 
 from .models import Property, PropertyImage
 from .forms import PropertyForm
@@ -9,8 +10,59 @@ from .forms import PropertyForm
 def property_list(request):
     properties = Property.objects.all()
 
+    search_query = request.GET.get("search", "")
+    min_price = request.GET.get("min_price", "")
+    max_price = request.GET.get("max_price", "")
+    property_type = request.GET.get("property_type", "")
+    city = request.GET.get("city", "")
+    min_bedrooms = request.GET.get("min_bedrooms", "")
+
+    if search_query:
+        properties = properties.filter(
+            title__icontains=search_query
+        )
+
+    if min_price:
+        properties = properties.filter(
+            price__gte=min_price
+        )
+
+    if max_price:
+        properties = properties.filter(
+            price__lte=max_price
+        )
+
+    if property_type:
+        properties = properties.filter(
+            property_type=property_type
+        )
+
+    if city:
+        properties = properties.filter(
+            city__icontains=city
+        )
+
+    if min_bedrooms:
+        properties = properties.filter(
+            bedrooms__gte=min_bedrooms
+        )
+
+    paginator = Paginator(properties, 6)
+
+    page_number = request.GET.get("page")
+
+    page_obj = paginator.get_page(page_number)
+
     context = {
-        "properties": properties,
+        "properties": page_obj,
+        "page_obj": page_obj,
+        "search_query": search_query,
+        "min_price": min_price,
+        "max_price": max_price,
+        "property_type": property_type,
+        "city": city,
+        "min_bedrooms": min_bedrooms,
+        "property_types": Property.PropertyType.choices,
     }
 
     return render(request, "properties/property_list.html", context)
